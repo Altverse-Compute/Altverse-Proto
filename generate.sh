22ddcd350e55ea870331226637427609a24ae2d4
EtherCD <ethernetcoder@gmail.com>
2025-12-25 19:32:01 +0200

    Attempt to assemble proto services

--- generate.sh
+++ generate.sh
@@ -6,3 +6,6 @@ rm -rf generated/*
 mkdir -p generated/js
 pbjs -t static-module -w es6 -o generated/js/index.js proto/game.proto
 pbts -o generated/js/index.d.ts generated/js/index.js
+
+pbjs -t static-module -w es6 -o generated/js/rpc.js proto/rpc.proto
+pbts -o generated/js/rpc.d.ts generated/js/rpc.js

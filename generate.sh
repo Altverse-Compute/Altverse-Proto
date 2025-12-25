26de79e74ecbe33d6246c8d155eceb1b3ac157f1
EtherCD <ethernetcoder@gmail.com>
2025-12-25 18:46:37 +0200

    Base for grpc connection between service and server

--- generate.sh
+++ generate.sh
@@ -4,5 +4,5 @@ set -e
 rm -rf generated/*
 
 mkdir -p generated/js
-pbjs -t static-module -w es6 -o generated/js/index.js proto/*.proto
+pbjs -t static-module -w es6 -o generated/js/index.js proto/game.proto
 pbts -o generated/js/index.d.ts generated/js/index.js

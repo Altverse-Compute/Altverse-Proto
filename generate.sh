bcd58c45c2e7dff8f2691e62666740fd6e0e9607
EtherCD <ethernetcoder@gmail.com>
2025-12-25 19:50:39 +0200

    Attempting to compile in grpc-js

--- generate.sh
+++ generate.sh
@@ -7,5 +7,5 @@ mkdir -p generated/js
 pbjs -t static-module -w es6 -o generated/js/index.js proto/game.proto
 pbts -o generated/js/index.d.ts generated/js/index.js
 
-pbjs -t static-module -w es6 -o generated/js/rpc.js proto/rpc.proto
+pbjs -t grpc-js -w es6 -o generated/js/rpc.js proto/rpc.proto
 pbts -o generated/js/rpc.d.ts generated/js/rpc.js

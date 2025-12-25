d31c77fde6938b5f0c59455f7302da19ba0a3770
EtherCD <ethernetcoder@gmail.com>
2025-12-25 22:51:31 +0200

    Trying to generate types for grpc

--- generate.sh
+++ generate.sh
@@ -4,8 +4,7 @@ set -e
 rm -rf generated/*
 
 mkdir -p generated/js
-pbjs -t static-module -w es6 -o generated/js/index.js proto/game.proto
+pbjs -t static-module -w es6 -o generated/js/index.js proto/*.proto
 pbts -o generated/js/index.d.ts generated/js/index.js
 
-pbjs -t grpc-js -w es6 -o generated/js/rpc.js proto/rpc.proto
-pbts -o generated/js/rpc.d.ts generated/js/rpc.js
+proto-loader-gen-types --longs=String --enums=String  --oneofs --grpcLib=@grpc/grpc-js --outDir=generated/ts proto/*.proto
